7a2c329147cac0347e51282b52019dda7eda8a6c
Runeth <runethpathirana@gmail.com>
2026-05-02 12:26:48 +0530

    initial commit

---
+++ src/main/java/com/quizapp/quizapp/HelloController.java
@@ -0,0 +1,19 @@
+package com.quizapp.quizapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HelloController {
+
+    @GetMapping("/hello")
+
+    public String sayHello() {
+
+        return "Hello! Your Quiz App Backend is running 🚀";
+
+    }
+
+}
